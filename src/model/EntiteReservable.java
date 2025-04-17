7358b934060e387e18918e85393e368fb181bef5
chakrialim <limchakriasaurphear@gmail.com>
2025-04-17 18:03:07 +0200

    debut tp 6

--- src/model/EntiteReservable.java
+++ src/model/EntiteReservable.java
@@ -1,5 +1,40 @@
 package model;
 
-public class EntiteReservable {
+public abstract class EntiteReservable <T extends Formulaire>{
+	CalendrierAnnuel calendrier;
+	int identification;
+	
+	public int getIdentification() {
+		return identification;
+	}
+	
+	public void setCalendrier(CalendrierAnnuel calendrier) {
+		this.calendrier = calendrier;
+	}
+	
+	public boolean estLibre(T formulaire) {
+		int jour = formulaire.getJour();
+		int mois = formulaire.getMois();
+		return calendrier.estLibre(jour, mois);
+	}
+	
+	public abstract boolean compatible(T formulaire);
+//	{
+//		int jour = formulaire.getJour();
+//		int mois = formulaire.getMois();
+//		return calendrier.reserver(jour, mois);
+//	}
+//	
+	public abstract Reservation reserver(T formulaire);
+//	{
+//		Reservation nvRes = null;
+//		int jour = formulaire.getJour();
+//		int mois = formulaire.getMois();
+//		if (compatible(formulaire)) {
+//			nvRes = new Reservation(jour, mois);
+//		}
+//		return nvRes;
+//	}
+	
 	
 }
--- src/model/ReservationRestaurant.java
+++ src/model/ReservationRestaurant.java
@@ -10,4 +10,22 @@ public class ReservationRestaurant extends Reservation {
 		this.numTable = numTable;
 	}
 
+	private String stringNumService() {
+		String texte;
+		switch (numService) {
+		case 1:
+			texte = "premier";
+			break;
+
+		default:
+			texte = "deuxi�me";
+		}
+		return texte;
+	}
+	
+	@Override
+	public String toString() {
+		return "Le " + super.getJour()+"/"+super.getMois()+"\nTable "+numTable+" pour le "+stringNumService()+" service." ;
+	}
+
 }
